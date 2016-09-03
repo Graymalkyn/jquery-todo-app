c33c7fd55cafabb62062142b428ef292815fb2e8
Mark Shuler <biggraydog@live.com>
2016-09-02 21:53:38 -0500

    finished product

--- public/script.js
+++ public/script.js
@@ -8,7 +8,20 @@ if (this.ToDo === undefined) this.ToDo = {};
 
   function getListItem(){
     itemEntry = $('#item-input');
-    ajaxCall();
+    var itemValue = itemEntry.val();
+    $.ajax({
+      url: '/api/todo',
+      method: 'POST',
+      data: {
+        text: itemValue,
+        isComplete: false
+      }
+    })
+
+    .done(function(result){
+      template(itemValue, result.id, false);
+    });
+
     itemEntry.val(' ');
   }
 
@@ -23,7 +36,7 @@ if (this.ToDo === undefined) this.ToDo = {};
     var templateFunc = _.template(templateHtml);
     var html = templateFunc(
       {
-        item: itemValue,
+        text: itemValue,
         guestId: id,
         className: className
       });
@@ -31,48 +44,41 @@ if (this.ToDo === undefined) this.ToDo = {};
   }
 
 
-
-  function ajaxCall() {
-    var itemValue = itemEntry.val();
+  function completedTask(evt){
+    var $target = $(evt.target);
+    // console.log('$target', $target); //{img.pencil}
+    // $target.addClass('completed');
+    var id = $target.data('id');
+    // console.log('id', id);            //random number
+    var text = $target.data('text');
+    // console.log('text', text);        //undefined
     $.ajax({
-      url: '/api/todo',
-      method: 'POST',
-      data: {
-        text: itemValue,
-        isComplete: false
-      }
-    })
+      url: 'api/todo/' + id,
+      method: 'PUT',
+        data: {
+          text: text,
+          id: id,
+          isComplete: true
+        }
+    });
 
-    .done(function(result){
-      template(itemValue, result.id, false);
-    }); //done
+    $target.toggleClass('completed');
   }
 
+
   function deleteClicked(evt) {
+    evt.stopPropagation();
     var $target = $(evt.target);
-    var id = $target.data('id');
-    console.log('id', id);
-    // alert('You have just deleted item number:' + id);
+    var id = $target.parent().data('id');
 
     $.ajax({
       url: '/api/todo/' + id,
       method: 'DELETE'
     });
 
-    $target.parent().remove();
+      $target.parent().remove();
   }
 
-  function completedTask(evt){
-    var $target = $(evt.target);
-    var id = $target.data('id');
-
-    $.ajax({
-      url: 'api/todo' + id,
-      method: 'PUT'
-    })
-
-    $target.parent().toggleClass('completed');
-  }
 
   function start() {
 
@@ -84,18 +90,20 @@ if (this.ToDo === undefined) this.ToDo = {};
 
     $('.list').on('click','.trash-can', deleteClicked),
 
-    $('.list').on('click','.pencil', completedTask),
+    $('.list').on('click','li', completedTask),
+
 
 
     $.ajax({
       url: '/api/todo',
       method: 'GET',
     })
-    .done(function(data){
-      for (var i=0; i<data.list.length; i++){
-        template(data.list[i].text, data.list[i].id, true)
+    .done(function(GETdata){
+      for (var i=0; i<GETdata.list.length; i++){
+        template(GETdata.list[i].text, GETdata.list[i].id, GETdata.list[i].isComplete);
       }
     });
+
   };
 
   context.start = start;
